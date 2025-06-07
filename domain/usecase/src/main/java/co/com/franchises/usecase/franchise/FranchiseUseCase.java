997d2056d868c3c0e5820ed97087e70811844ae5
Cristhian Andres Moreno Velasco <Cristhian.452@outlook.com>
2025-06-07 12:33:31 -0500

    Basic UseCases

---
+++ domain/usecase/src/main/java/co/com/franchises/usecase/franchise/FranchiseUseCase.java
@@ -0,0 +1,6 @@
+package co.com.franchises.usecase.franchise;
+
+import lombok.RequiredArgsConstructor;
+@RequiredArgsConstructor
+public class FranchiseUseCase {
+}
---
+++ domain/usecase/src/main/java/co/com/franchises/usecase/product/ProductUseCase.java
@@ -0,0 +1,6 @@
+package co.com.franchises.usecase.product;
+
+import lombok.RequiredArgsConstructor;
+@RequiredArgsConstructor
+public class ProductUseCase {
+}
